9f889e83c761d0d03c50a7e5671eae2f4e1b01c0
Raed <raed_nb@hotmail.com>
2019-07-22 10:49:44 +0300

    about finished

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import "./App.css";
-import Footer from "./components/Footer/footer";
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer/footer';
+import About_Us from './components/About_Us/About_Us.test';
+import './components/About_Us/About_Us.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<About_Us />
+		</div>
+	);
 }
 
 export default App;
