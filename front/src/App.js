4753a57a3bf7b0e8e32580ef0995d9f6c982bac0
raed <testuser@example.com>
2019-07-23 11:50:49 +0300

    about_us_fixed

--- front/src/App.js
+++ front/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Footer from './components/Footer/footer';
 import About_Us from './components/About_Us/About_Us.test';
 import './components/About_Us/About_Us.css';
+import Contact_Info from './components/Contact_Info/Contact_Info.test';
+import './components/Contact_Info/Contact_Info.css'
 
 function App() {
 	return (
 		<div className="App">
 			<About_Us />
+			{/* <Contact_Info /> */}
+
 		</div>
 	);
 }
--- front/src/components/About_Us/About_Us.test.js
+++ front/src/components/About_Us/About_Us.test.js
@@ -31,20 +31,21 @@ class About_Us extends Component {
 				<h1>About Us</h1>
 				<div className="About_Us_Icons">
 					<div className="History">
-						{this.state.toggleHistory && (
-							<p>
+						{this.state.toggleHistory ?
+							<p onMouseLeave={this.displayOff} >
 								WashyWash is a company that is set out to change how people clean their clothes
 								globally, switching people from the outdated PERC Dry Clean system to our EcoClean
 								technology.
 							</p>
-						)}
+						
+						:
 						<img
-							src={require('/home/raed/Documents/codi-docs/raed_projects/WashyWash/front/src/assets/images/histo.ico')}
+							src={require('/home/raed/Documents/codi_docs/raed_projects/WashyWash/front/src/assets/images/histo.ico')}
 							className="historyicon"
 							onMouseOver={this.displayHistory}
-							onMouseLeave={this.displayOff}
+							// onMouseLeave={this.displayOff}
 						/>
-
+						}
 						{/* <figure class="swap-on-hover">
  <img  class="swap-on-hover__front-image" src="https://c402277.ssl.cf1.rackcdn.com/photos/1620/images/carousel_small/bengal-tiger-why-matter_7341043.jpg?1345548942"/>
   <img class="swap-on-hover__back-image" src="http://www.menucool.com/slider/jsImgSlider/images/image-slider-2.jpg"/>
@@ -52,35 +53,38 @@ class About_Us extends Component {
 					</div>
 
 					<div className="Mission">
-						{this.state.toggleMission && (
-							<p>
+						{this.state.toggleMission?
+							<p onMouseLeave={this.displayOff}>
 								WashyWash is a company that is set out to change how people clean their clothes
 								globally, switching people from the outdated PERC Dry Clean system to our EcoClean
 								technology.
 							</p>
-						)}
+						:
 						<img
-							src={require('/home/raed/Documents/codi-docs/raed_projects/WashyWash/front/src/assets/images/mission.png')}
+							src={require('/home/raed/Documents/codi_docs/raed_projects/WashyWash/front/src/assets/images/mission.png')}
 							className="missionicon"
 							onMouseOver={this.displayMission}
-							onMouseLeave={this.displayOff}
+							// onMouseLeave={this.displayOff}
 						/>
+						}
 					</div>
 					<div className="Vision">
-						{this.state.toggleVision && (
-							<p>
+						{this.state.toggleVision ? 
+							<p 	onMouseLeave={this.displayOff}>
 								WashyWash is a company that is set out to change how people clean their clothes
 								globally, switching people from the outdated PERC Dry Clean system to our EcoClean
 								technology.
 							</p>
-						)}
+						:
 						<img
-							src={require('/home/raed/Documents/codi-docs/raed_projects/WashyWash/front/src/assets/images/vision.png')} className="visionicon"
+							src={require('/home/raed/Documents/codi_docs/raed_projects/WashyWash/front/src/assets/images/vision.png')} className="visionicon"
 							onMouseOver={this.displayVision}
-							onMouseLeave={this.displayOff}
+							// onMouseLeave={this.displayOff}
 						/>
+						}
 					</div>
 				</div>
+			
 			</div>
 		);
 	}
