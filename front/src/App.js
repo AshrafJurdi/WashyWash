78a703caed6260ff2fc04d5ccf61107bc929984d
omar-mhmd <mouri700@gmail.com>
2019-07-24 12:23:13 +0300

    card details

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Form from './components/Contact_Form/Contact_Form';
+import CardDetails from './components/CardDetails/CardDetails';
 
 function App() {
 	return (
@@ -9,7 +10,7 @@ function App() {
 				
 			</Router> */}
 
-			<Form />
+			<CardDetails/>
 		</div>
 	);
 }
