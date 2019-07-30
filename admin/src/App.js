368af743878c26de4521504daf541337d39aefe7
AshrafJurdi <ashrafjurdi@gmail.com>
2019-07-30 12:56:56 +0300

    cams navs

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import "./App.css";
-import Orders from "./Components/Orders/Orders.test";
-import CMSLoginForm from "./Components/Login/cmslogin";
+// import "./App.css";
+// import Orders from "./Components/Orders/Orders.test";
+// import CMSLoginForm from "./Components/Login/cmslogin";
+import Header from "./Components/Header/header";
+import NavBar from "./Components/NavBar/navbar";
 
 function App() {
   return (
     <div className="App">
-      <CMSLoginForm />
+      <Header />
+      <NavBar />
     </div>
   );
 }
