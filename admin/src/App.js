3471e1b7ad95e6dfe6a8ae3a4cd1b2baa6984c92
raed <testuser@example.com>
2019-08-01 11:33:44 +0300

    queryselector

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-import Orders from "./Components/Orders/Orders.test";
-import Orders_Updated from "./Components/Orders_Updated/Orders_Updated";
-import Users from "./Components/Users/Users";
+// import { Route, withRouter } from "react-router-dom";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Orders_Updated from "./Components/Orders_Updated/Orders_Updated";
+// import Users from "./Components/Users/Users";
 import Products from "./Components/Products/Products";
 // import User_Popup from "../../front/src/components/User_Popup/User_Popup.test";
-import "./App.css";
-import Orders from "./Components/Orders/Orders.test";
-import CMSLoginForm from "./Components/Login/cmslogin";
+
+// import Orders from "./Components/Orders/Orders.test";
+// import CMSLoginForm from "./Components/Login/cmslogin";
 
 function App() {
   return (
     <div className="App">
-      {/* <Orders_Updated /> */}
-      {/* <Users /> */}
+      {/* <Route path="/" render={props => <Products {...props} />} /> */}
       <Products />
-      <CMSLoginForm />
     </div>
   );
 }
-
 export default App;
+// export default withRouter(App);
--- admin/src/Components/Products/Products.js
+++ admin/src/Components/Products/Products.js
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
-import { Card, CardDeck, Button, ButtonToolbar } from "react-bootstrap";
+import { Card, CardDeck, Button, ButtonToolbar, Form } from "react-bootstrap";
 import "../Products/Products.css";
+import axios from "axios";
 
 export default class Products extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      toggleMainPage: false,
+
       clothes: [
         {
-          Image:
-            "/home/raed/Desktop/WashyWash/admin/src/Assets/Images/jeans.jpeg",
+          Image: "jeans.jpg",
           Name: "Jeans",
           Price: "33"
         },
         {
-          Image:
-            "/home/raed/Desktop/WashyWash/admin/src/Assets/Images/shirt.jpg",
+          Image: "shirt.jpg",
           Name: "Shirt",
           Price: "15"
         },
         {
-          Image:
-            "/home/raed/Desktop/WashyWash/admin/src/Assets/Images/socks.jpg",
+          Image: "socks.jpg",
           Name: "socks",
           Price: "15"
         }
@@ -36,66 +36,149 @@ export default class Products extends React.Component {
     this.setState({ clothes });
   };
   editProducts = index => {
-    let clothes = this.state.clothes;
+    let clothes = [...this.state.clothes];
 
-    clothes[index].Name = prompt("Please edit Name");
-    clothes[index].Price = prompt("Please edit Price");
+    const name = prompt("Please edit Name");
+    if (name) {
+      clothes[index].Name = name;
+
+      const price = prompt("Please edit Price");
+      if (price) clothes[index].Price = price;
+    }
+
     this.setState({ clothes });
   };
-  // addProducts = () => {
+  showPop = () => {
+    this.setState({ toggleMainPage: true });
+  };
+  addProducts = e => {
+    let clothes = this.state.clothes;
+    e.preventDefault();
+    var a = { Name: "", Price: "" };
+    a.Name = e.target.Product_Name.value;
+    a.Price = e.target.Product_Price.value;
+    clothes.push(a);
+    this.setState({ toggleMainPage: false, clothes });
+    // console.log(a);
+
+    // this.props.history.push("/#");
+  };
+  fileSelectHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  };
+  // fileUploadHandler = e => {
+  //   // const fd = new FormData();
+  //   // fd.append("image", this.state.selectedFile, this.state.selectedFile.name);
   //   let clothes = this.state.clothes;
-  //   var a = { Name: "", Price: "" };
-  //   console.log(clothes);
-  //   a.Name = prompt("Please add Name");
-  //   a.Price = prompt("Please add Price");
-  //   clothes.push(a);
+  //   var b = { Name: "", Price: "", selectedFile: "" };
+  //   // b.Name = e.target.Product_Name.value;
+  //   // b.Price = e.target.Product_Price.value;
+  //   clothes.push(b);
   //   this.setState({ clothes });
+  //   ////////////////////////////
+  //   axios
+  //     .post(
+  //       "https://us-central1-fb-cloud-functions-demo.cloudfunctions.net/uploadFile",
+  //       clothes
+  //     )
+  //     .then(res => {
+  //       console.log(res);
+  //     });
   // };
 
   render() {
     return (
       <div className="Products">
-        <ButtonToolbar className="Button_Add_Products">
-          <Button
-            variant="outline-dark"
-            className="Button_Addproduct"
-            onClick={this.addProducts}
-          >
-            <a class="button" href="#popup1">
-              Add Product
+        <div>
+          <ButtonToolbar className="Button_Add_Products">
+            <a className="button" href="#popup1">
+              <Button
+                variant="outline-dark"
+                className="Button_Addproduct"
+                onClick={this.showPop}
+              >
+                Add Product
+              </Button>
             </a>
-          </Button>
-        </ButtonToolbar>
+          </ButtonToolbar>
 
-        <div className="Cards_Products">
-          {this.state.clothes.map((item, index) => (
-            <CardDeck>
-              <Card className="Products_Card">
-                <Card.Img
-                  className="Card_Image"
-                  variant="top"
-                  src={require("/home/raed/Desktop/WashyWash/admin/src/Assets/Images/jeans.jpeg")}
-                />
-                <Card.Body className="Card_Body">
-                  <Card.Text>Name:{item.Name}</Card.Text>
-                  <Card.Text>Price:{item.Price}</Card.Text>
-                  <div className="Icons">
-                    <span>
-                      <i
-                        class="fas fa-trash-alt"
-                        onClick={() => this.deleteProducts(index)}
-                      />
-                    </span>
+          <div className="Cards_Products">
+            {this.state.clothes.map((item, index) => (
+              <CardDeck>
+                <Card className="Products_Card">
+                  <Card.Img
+                    className="Card_Image"
+                    variant="top"
+                    src={item.Image}
+                  />
+                  <Card.Body className="Card_Body">
+                    <Card.Text>Name:{item.Name}</Card.Text>
+                    <Card.Text>Price:{item.Price}</Card.Text>
+                    <div className="Icons">
+                      <span>
+                        <i
+                          class="fas fa-trash-alt"
+                          onClick={() => this.deleteProducts(index)}
+                        />
+                      </span>
 
-                    <span onClick={() => this.editProducts(index)}>
-                      <i class="fas fa-edit" />
-                    </span>
-                  </div>
-                </Card.Body>
-              </Card>
-            </CardDeck>
-          ))}
+                      <span onClick={() => this.editProducts(index)}>
+                        <i class="fas fa-edit" />
+                      </span>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </CardDeck>
+            ))}
+          </div>
         </div>
+        {this.state.toggleMainPage && (
+          <div id="popup1" className="overlay_AddProduct">
+            <div className="popup_AddProduct">
+              <a className="close" href="#">
+                &times;
+              </a>
+              <div className="content">
+                <form onSubmit={e => this.addProducts(e)}>
+                  <div className="Inputs_AddProduct">
+                    <input
+                      type="file"
+                      placeholder="Upload Image"
+                      onChange={this.fileSelectHandler}
+                    />
+                    {/* <button onClick={e => this.fileUploadHandler(e)}>
+                      Upload Image
+                    </button> */}
+                    <input
+                      placeholder="Product Name"
+                      // className="Product_Name"
+                      name="Product_Name"
+                      required
+                    />
+                    <input
+                      name="Product_Price"
+                      placeholder="Product Price"
+                      required
+                      // className="Product_Price"
+                    />
+                  </div>
+                  <div className="Buttons_AddProduct">
+                    {/*   <button variant="outline-dark" onClick={this.addProducts}>
+                    Create
+                  </button> */}
+                    <input
+                      type="submit"
+                      value="Create"
+                      // onSubmit={e => this.addProducts(e)}
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
